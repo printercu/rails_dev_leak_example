746eee555b58e99ef2f4db48e58aee7a45cd34e0
Max Melentiev <melentievm@gmail.com>
2016-12-23 15:04:38 +0300

    simple page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  after_action do
+    GC.start
+    stat = GC.stat
+    logger.info "Heap slots: #{stat[:heap_live_slots]}/#{stat[:heap_available_slots]}"
+  end
 end
